524ffd2010beda20ead9d112c3afcdc7b8a12587
SeanCGriffin <sean.c.griffin@gmail.com>
2021-03-01 16:40:36 -0500

    Fixing component stuff so the project synthesizes.

--- hdl/heartbeat.vhd
+++ hdl/heartbeat.vhd
@@ -7,27 +7,27 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity heartbeat is
+entity heartbeat_pulser is
     generic (
         PERIOD: positive := 100000000
     );
     port (
         clk       : in std_logic  := '0';
-        rst       : in std_logic  := '0';
+        rst_n     : in std_logic  := '1';
         heartbeat : out std_logic := '0'
     );
 
 end entity;
 
-architecture rtl of heartbeat is
+architecture rtl of heartbeat_pulser is
 
     signal counter : unsigned(32-1 downto 0) := (others=>'0');
 
 begin
 
-    count : process( rst, clk )
+    count : process( rst_n, clk )
     begin
-        if rst = '1' then
+        if rst_n = '1' then
             counter <= (others=>'0');
             heartbeat <= '0';
         elsif rising_edge(clk) then
